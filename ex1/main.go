fb793b1b1a942bc0d16dfbb604fa2b6149391059
Lucas Marques <lucas.smarques@outlook.com>
2018-06-02 16:03:23 -0300

    Exercise 1 - Part 2

--- ex1/main.go
+++ ex1/main.go
@@ -9,40 +9,72 @@ import (
 	"log"
 	"os"
 	"strings"
+	"time"
 )
 
+// Question holds the question text and its answer
 type Question struct {
 	Text   string
 	Answer string
 }
 
+// Control holds game informations
+type Control struct {
+	Corrects       int
+	Timeout        bool
+	TotalQuestions int
+}
+
+// Missed results on how many questions the gamer missed
+func (c Control) Missed() int {
+	return c.TotalQuestions - c.Corrects
+}
+
 func main() {
 	quiz := flag.String("quiz", "problems.csv", "Optional. Define alternative problems to the quiz.")
+	duration := flag.Int("timer", 30, "In seconds. The maxium time to complete the quiz.")
 	flag.Parse()
+
 	questions, err := parseCSV(*quiz)
 	if err != nil {
 		log.Fatalln(err)
 	}
 
-	control := struct {
-		Corrects int
-		Wrongs   int
-	}{Corrects: 0, Wrongs: 0}
+	timer := time.NewTimer(time.Duration(*duration) * time.Second)
+	control := Control{TotalQuestions: len(*questions)}
 
 	fmt.Println("[[[ Welcome to Quiz Game ]]]")
 	reader := bufio.NewReader(os.Stdin)
+
+GameLoop:
 	for idx, q := range *questions {
 		fmt.Printf("(Question %d) %s ? ", idx+1, q.Text)
-		answer, _ := reader.ReadString('\n')
-		answer = answer[:len(answer)-1]
-		if strings.Compare(answer, q.Answer) == 0 {
-			control.Corrects += 1
-		} else {
-			control.Wrongs += 1
+		answerCh := make(chan string)
+
+		go func() {
+			answer, _ := reader.ReadString('\n')
+			answerCh <- answer[:len(answer)-1]
+		}()
+
+		select {
+		case <-timer.C:
+			control.Timeout = true
+			break GameLoop
+		case answer := <-answerCh:
+			if strings.Compare(answer, q.Answer) == 0 {
+				control.Corrects++
+			}
 		}
 	}
 
-	fmt.Printf("You got %d questions and missed %d\n", control.Corrects, control.Wrongs)
+	gameOver(control)
+}
+
+func gameOver(c Control) {
+	if c.Timeout {
+		fmt.Printf("\nYour time is gone!\n")
+	}
+	fmt.Printf("You got %d of questions and missed %d\n", c.Corrects, c.Missed())
 }
 
 func parseCSV(filename string) (*[]Question, error) {
